9784448b7c7a96d95953fef6cd9be9d92937ff53
anesterov <nesterov.alexander@outlook.com>
2024-09-16 19:33:06 +0200

    fix sanitizr 2

--- modules/core/task/func_tests/task_tests.cpp
+++ modules/core/task/func_tests/task_tests.cpp
@@ -68,27 +68,27 @@ TEST(task_tests, check_double) {
   EXPECT_NEAR(out[0], static_cast<double>(in.size()), 1e-6);
 }
 
-TEST(task_tests, check_uint8_t) {
+TEST(task_tests, check_int8_t) {
   // Create data
-  std::vector<uint8_t> in(20, 1);
-  std::vector<uint8_t> out(1, 0);
+  std::vector<int8_t> in(20, 1);
+  std::vector<int8_t> out(1, 0);
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskData = std::make_shared<ppc::core::TaskData>();
-  taskData->inputs.emplace_back(in.data());
+  taskData->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
   taskData->inputs_count.emplace_back(in.size());
-  taskData->outputs.emplace_back(out.data());
+  taskData->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
   taskData->outputs_count.emplace_back(out.size());
 
   // Create Task
-  auto testTask = std::make_shared<ppc::test::TestTask<uint8_t>>(taskData);
-  bool isValid = testTask->validation();
+  ppc::test::TestTask<int8_t> testTask(taskData);
+  bool isValid = testTask.validation();
   ASSERT_EQ(isValid, true);
 
-  testTask->pre_processing();
-  testTask->run();
-  testTask->post_processing();
-  ASSERT_EQ(out[0], static_cast<uint8_t>(in.size()));
+  testTask.pre_processing();
+  testTask.run();
+  testTask.post_processing();
+  ASSERT_EQ(out[0], static_cast<int8_t>(in.size()));
 }
 
 TEST(task_tests, check_int64_t) {
